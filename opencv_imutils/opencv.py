36830edc5bc51310caea3f59711fc1f7a1a34a01
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-10-21 11:56:35 +0530

    output all faces from all frames

--- opencv_imutils/opencv.py
+++ opencv_imutils/opencv.py
@@ -1,26 +1,35 @@
 import cv2
-# import sys
 
-# imagePath = sys.argv[1]
+vidPath = r"C:\Users\daksh\Desktop\VSCode\SurveillanceBot\cnn_facial_reco\opencv_imutils\vid.mp4"
 
-image = cv2.imread("faces.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+video = cv2.VideoCapture(vidPath)
+length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+print(length)
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.3,
-    minNeighbors=3,
-    minSize=(30, 30)
-)
+while True:
+ 
+    # Capture frame-by-frame
+    ret, frame = video.read()
+    if ret == False:
+        break
 
-print("[INFO] Found {0} Faces.".format(len(faces)))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    roi_color = image[y:y + h, x:x + w]
-    print("[INFO] Object found. Saving locally.")
-    cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+    )
 
-status = cv2.imwrite('faces_detected.jpg', image)
-print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
+    print("[INFO] Found {0} Faces.".format(len(faces)))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_color = frame[y:y + h, x:x + w]
+        print("[INFO] Object found. Saving locally.")
+        cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
+
+    status = cv2.imwrite('faces_detected.jpg', frame)
+    print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
